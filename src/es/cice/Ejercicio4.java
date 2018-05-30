adda0531ba3dfb933b09347a084ce49197a852ec
imarlebana <imarlebana@eurostar>
2018-05-30 21:45:18 +0200

    Ejercicio 4

--- src/es/cice/Ejercicio4.java
+++ src/es/cice/Ejercicio4.java
@@ -1,4 +1,23 @@
 package es.cice;
 
+import java.util.Scanner;
+
 public class Ejercicio4 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Introduce un numero:");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int factorial = 0;
+
+        if (num >0){
+            factorial = 1;
+            for (int i = num; i > 0; i-- ){
+                factorial = factorial *i;
+            }
+        }
+        System.out.println("el factorial es: " + factorial);
+
+    }
 }
