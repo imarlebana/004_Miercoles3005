c50870e3cd9b42686922a02a4cc3a4ff724d32af
imarlebana <imarlebana@eurostar>
2018-05-30 19:35:01 +0200

    esta roto

--- src/es/cice/Saco.java
+++ src/es/cice/Saco.java
@@ -4,5 +4,13 @@ public class Saco {
 
     public static void main(String[] args) {
 	// write your code here
+
+        //ARRAY UNIDIMENSIONALES
+        int[] arrayUni = new int[2];
+
+        //ARRAY BIDIMENSIONALES
+        int [][] bidimensional = new int[2][4];
+        int [][] bidimensional2 = {{0,1,2,3},{0,1,2,3}};
+
     }
 }
