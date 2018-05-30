948f93dedcdaf1051110d0be372ffe099b330443
imarlebana <imarlebana@eurostar>
2018-05-30 19:53:07 +0200

    concionales

---
+++ src/es/cice/Condicionales.java
@@ -0,0 +1,15 @@
+package es.cice;
+
+public class Condicionales {
+
+    public static void main(String[] args) {
+
+        //ESTRUCTURA IF / IF_ELSE
+        int a = (int) Math.random();
+        if(a%2==0){
+            System.out.println("Holi");
+        }else{
+            System.out.println("dew");
+        }
+    }
+}
