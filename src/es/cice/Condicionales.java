f3106a31adfe9203cfacb50f65b2d96b0214a265
imarlebana <imarlebana@eurostar>
2018-05-30 20:52:22 +0200

    switch

--- src/es/cice/Condicionales.java
+++ src/es/cice/Condicionales.java
@@ -11,5 +11,18 @@ public class Condicionales {
         }else{
             System.out.println("dew");
         }
+
+
+        //switch
+        switch(2/5){
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            default:
+                System.out.println("default");
+        }
     }
 }
